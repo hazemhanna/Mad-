2ab71047d0603643872b66ac3cb6eae9391f5ee4
hazemhanna <37251267+hazemhanna@users.noreply.github.com>
2021-05-10 12:17:49 +0200

    last update

--- Mad/AppDelegate.swift
+++ Mad/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var token = Helper.getAPIToken()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if token != nil {
+        if token != "" {
             let sb = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CardTabBarController")
             window?.rootViewController = sb
         }else {
