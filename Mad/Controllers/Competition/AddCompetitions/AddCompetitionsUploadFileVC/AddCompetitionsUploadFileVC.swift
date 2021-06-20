30b3848a7a5f7a4686b9a5551b8883b47a788779
hazemhanna <37251267+hazemhanna@users.noreply.github.com>
2021-06-20 19:59:44 +0200

    add file picker

--- Mad/Controllers/Competition/AddCompetitions/AddCompetitionsUploadFileVC/AddCompetitionsUploadFileVC.swift
+++ Mad/Controllers/Competition/AddCompetitions/AddCompetitionsUploadFileVC/AddCompetitionsUploadFileVC.swift
@@ -9,7 +9,8 @@ import UIKit
 import RxSwift
 import RxCocoa
 import PTCardTabBar
-
+import MobileCoreServices
+import UniformTypeIdentifiers
 
 class AddCompetitionsUploadFileVC: UIViewController {
 
@@ -44,7 +45,38 @@ class AddCompetitionsUploadFileVC: UIViewController {
         let vc = SubmitCopetitionsVC.instantiateFromNib()
         self.navigationController?.pushViewController(vc!, animated: true)
     }
-
     
+    @IBAction func uploadButton(sender: UIButton) {
+        print("hazem")
+        self.clickFunction()
+    }
+    
+}
+
+extension AddCompetitionsUploadFileVC : UIDocumentPickerDelegate,UINavigationControllerDelegate{
+    func clickFunction(){
+        let importMenu = UIDocumentPickerViewController(documentTypes: ["com.microsoft.word.doc","org.openxmlformats.wordprocessingml.document", kUTTypePDF as String], in: UIDocumentPickerMode.import)
+        importMenu.delegate = self
+        importMenu.modalPresentationStyle = .formSheet
+        self.present(importMenu, animated: true, completion: nil)
+    }
+    
+    
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let myURL = urls.first else {
+            return
+        }
+        print("import result : \(myURL)")
+    }
+          
+    public func documentMenu(_ documentMenu:UIDocumentPickerViewController, didPickDocumentPicker documentPicker: UIDocumentPickerViewController) {
+        documentPicker.delegate = self
+        present(documentPicker, animated: true, completion: nil)
+    }
+    
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print("view was cancelled")
+        dismiss(animated: true, completion: nil)
+    }
     
 }
